01c9a576cfbd5e5e229cc1b63464fe90d749d2f6
Rohit <punekar435@gmail.com>
2024-02-15 13:30:12 +0530

    code changes by GitLearning

---
+++ src/section14/Practice_4.java
@@ -0,0 +1,12 @@
+package section14;
+
+public class Practice_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("New class added by GitLearning");
+
+	}
+
+}
