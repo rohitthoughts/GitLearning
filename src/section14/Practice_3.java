a33e2555bb27eb17f0657df05135ec87f611915a
Rohit <punekar435@gmail.com>
2024-02-15 12:12:18 +0530

    New code added by newRepo

---
+++ src/section14/Practice_3.java
@@ -0,0 +1,12 @@
+package section14;
+
+public class Practice_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I have added this code");
+
+	}
+
+}
