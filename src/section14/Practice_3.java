ca677ff69f7b8e93b8748ff184ff231353470214
Rohit <punekar435@gmail.com>
2024-02-15 13:21:28 +0530

    changes made by gitDemo

--- src/section14/Practice_3.java
+++ src/section14/Practice_3.java
@@ -4,11 +4,23 @@ public class Practice_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("I have added this code");
-		
+
 		System.out.println("Code is again added by ExisitngRepo");
 
+	}
+
+	public void method1() {
+		System.out.println("Hello world");
+	}
+
+	public void method2() {
+		System.out.println("Hello world");
+	}
+
+	public void method3() {
+		System.out.println("Hello world");
 	}
 
 }
